a33d94647a5544cf2b2fa0d180e8efe21ea6ade1
Carolina Borim <carolina.borim@gmail.com>
2017-02-02 10:00:51 -0500

    Make SearchBar a controlled component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchBar';
 
 
+YTSearch({key: API_KEY, term: 'flute'}, function(data){
+  console.log(data);
+});
 
 const App = () => {
   return (
