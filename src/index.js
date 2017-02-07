c893c5b5d56218ac359bbe681ef65608c03c91f0
Carolina Borim <carolina.borim@gmail.com>
2017-02-06 23:21:48 -0500

    Changes implementation to allow video selection

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'flute'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -20,8 +26,10 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
-        <VideoDetails video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
