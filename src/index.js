84be7fc618fdf70e801ff24e9a0b4389a10b7094
Carolina Borim <carolina.borim@gmail.com>
2017-02-07 23:31:27 -0500

    Changes SearchBar to allow search :)

--- src/index.js
+++ src/index.js
@@ -14,18 +14,22 @@ class App extends Component{
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'flute'}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
+    this.videoSearch('flute');
   }
+
+videoSearch(term){
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  });
+}
 
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={ term => this.videoSearch(term)} />
         <VideoDetails video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({ selectedVideo})}
